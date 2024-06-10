d3942173231fa867fd9115424671aa5d0841feab
Fayyozbek <usmonovbahriddin108@gmail.com>
2024-06-10 10:39:46 +0500

    [ADD] loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,9 @@ function Home() {
   const { data,isPending,error} = useFetch("https://dummyjson.com/products")
   return (
     <div>
-      {isPending && <span className="loading loading-spinner loading-lg"></span>}
+      {isPending && <span className="loading loading-spinner w fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-40 h-40"></span>}
       {data && <ProductList data={data}/>}
+      {isPending && <div className="overlay fixed top-0 left-0 bottom-0 w-full bg-black/50"></div>}
     </div>
   )
 }
