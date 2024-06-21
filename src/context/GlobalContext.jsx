9946669597cdb942e612b991dd610992a36f6227
Fayyozbek <usmonovbahriddin108@gmail.com>
2024-06-21 11:06:45 +0500

    [Fix] immer bug

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useReducer } from "react";
+import { createContext, useReducer } from "react";
 export const GlobalContext = createContext();
 const changeState = (state, action) => {
   const { type, payload } = action;
@@ -9,29 +9,22 @@ const changeState = (state, action) => {
       return { ...state, user: null };
     case "AUTH_CHANGE":
       return { ...state, isAuthChange: true };
-    case "ADD_PRODUCT":
-      return { ...state, product: payload };
-    case "CHANGE_TOTAL":
-        return {...state,total:payload}
     default:
       return state;
   }
 };
 
 function GlobalContextProvider({ children }) {
-  const [state, dispatch] = useReducer(changeState, {
+  const [state, dispatch] = useReducer(changeState,{
     user: null,
     product: [],
     total: 0,
     isAuthChange: false,
-  } );
-  };
-  useEffect(()=>{
-    localStorage.setItem("mystore", JSON.stringify(state))
-  },[state])
+  });
   return (
-    <GlobalContext.Provider value={{ ...state, dispatch, addProduct }}>
+    <GlobalContext.Provider value={{ ...state, dispatch}}>
       {children}
     </GlobalContext.Provider>
   );
+}
 export default GlobalContextProvider;
